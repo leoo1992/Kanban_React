2bcc6a333331b30bebdca38962e54b36b8fe4f62
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-04-14 18:52:43 -0300

    Initial commit

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,45 @@
+import "./TaskList.css";
+import PropTypes from "prop-types";
+import TaskItem from "../TaskItem/TaskItem";
+
+export default function TaskList({
+  title,
+  taskState,
+  onAddTask,
+  tasks,
+  onTaskUpdate,
+  onDeleteTask
+}) {
+  const addTask = () => {
+    onAddTask("Editar Tarefa", taskState);
+  };
+
+  return (
+    <div className="TaskList">
+      <div className="title">{title}</div>
+      <div className="content">
+        {tasks.map((task) => {
+          return (
+            <TaskItem
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              taskState={task.state}
+              onTaskUpdate={onTaskUpdate}
+              onDeleteTask={onDeleteTask}
+            />
+          );
+        })}
+      </div>
+      <button className="button-85" onClick={addTask}>
+        +
+      </button>
+    </div>
+  );
+}
+
+TaskList.propTypes = {
+  title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired
+};
